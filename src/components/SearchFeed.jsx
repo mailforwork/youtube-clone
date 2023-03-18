47743a7c873e60c0a5cfef5a781c605e0e854d3d
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-03-18 12:04:46 +0530

    🎶🎼Implemented Seach Function FINALLY💣💫

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const SearchFeed = () => {
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
+import { Videos } from "./";
+
+const Feed = () => {
+  // const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    // setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
-  )
-}
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          Follow are search results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+        </Typography>
 
-export default SearchFeed
+        <Videos videos={videos} />
+      </Box>
+  );
+};
+
+export default Feed;
