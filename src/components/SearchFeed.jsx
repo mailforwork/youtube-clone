7454b9057657cef3fc1e5fafaab12f6853604d5e
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-03-18 13:01:54 +0530

    🫡🙋‍♀️FIXED CODE FORMATTING🔨🚧

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,36 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Typography, Box } from "@mui/material";
+import { useParams } from "react-router-dom";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { useParams } from "react-router-dom";
 import { Videos } from "./";
 
 const SearchFeed = () => {
-  // const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState(null);
   const { searchTerm } = useParams();
 
   useEffect(() => {
-    // setVideos(null);
-
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
       setVideos(data.items)
     );
   }, [searchTerm]);
 
   return (
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          mb={2}
-          sx={{ color: "white" }}
-        >
-          Follow are search results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
-        </Typography>
-
-        <Videos videos={videos} />
+    <Box p={2} minHeight="95vh">
+      <Typography
+        variant="h4"
+        fontWeight={900}
+        color="white"
+        mb={3}
+        ml={{ sm: "100px" }}
+      >
+        Search Results for{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        {<Videos videos={videos} />}
       </Box>
+    </Box>
   );
 };
 
